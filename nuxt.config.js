b20c5c01eb8ed861188f72a8ed52806420b886e6
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-07-05 14:57:21 +0800

    add normalizecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  srcDir: 'nuxt/'
+  srcDir: 'nuxt/',
+  head: {
+    link: [
+      {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css'}
+    ]
+  }
 }
